f9cf679e30c3472891ba23a2ffed541d826be5cf
Prableen Kaur <67010590+prableen14@users.noreply.github.com>
2020-12-15 14:56:27 +0530

    Add files via upload

---
+++ tic tac toe/j.js
@@ -0,0 +1,24 @@
+var restart= document.querySelector("#b")
+var squares= document.querySelectorAll('td')
+function clearboard() {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].textContent = '';
+  }
+
+}
+restart.addEventListener('click',clearboard);
+function changeMarker() {
+if ( this.textContent==='') {
+  this.textContent='X';
+}
+else if (this.textContent==='X') {
+  this.textContent='0';
+
+}
+else {
+  this.textContent='';
+}
+}
+for (var i = 0; i < squares.length; i++) {
+  squares[i].addEventListener('click',changeMarker);
+}
